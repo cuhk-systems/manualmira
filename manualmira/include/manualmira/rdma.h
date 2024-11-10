eb61f109671b3c62ddcd5044d2d99003cf6e016d
Yau-ming Leung <ymleung314@gmail.com>
2024-11-10 20:18:45 +0800

    refactor: replace RDMA CM EP with IBV

--- manualmira/include/manualmira/rdma.h
+++ manualmira/include/manualmira/rdma.h
@@ -8,52 +8,132 @@
 
 #include "infiniband/verbs.h"
 #include "rdma/rdma_cma.h"
+#include "rdma/rdma_verbs.h"
 
 namespace manualmira::rdma {
 
+namespace internal {
+
+rdma_cm_event* await_cm_event(rdma_event_channel* ch, rdma_cm_event_type event);
+void await_ack_cm_event(rdma_event_channel* ch, rdma_cm_event_type event);
+
+}  // namespace internal
+
+template <bool IsServer>
+class connection;
+
+using server_connection = connection<true>;
+using client_connection = connection<false>;
+
+template <bool IsServer>
 class connection {
  public:
   friend class server;
-  friend connection connect(const char* addr, const char* port);
+  friend client_connection connect(const char* addr, const char* port);
 
   connection(const connection&) = delete;
-  connection(connection&& other) noexcept;
 
-  ~connection();
+  connection(connection&& other) noexcept {
+    std::swap(evt_ch_, other.evt_ch_);
+    std::swap(id_, other.id_);
+
+    std::swap(pd_, other.pd_);
+    std::swap(cq_, other.cq_);
+
+    std::swap(mr_bufs_, other.mr_bufs_);
+
+    std::swap(addr_info_, other.addr_info_);
+  }
+
+  ~connection() {
+    if (IsServer) {
+      internal::await_ack_cm_event(evt_ch_, RDMA_CM_EVENT_DISCONNECTED);
+      rdma_disconnect(id_);
+    } else {
+      rdma_disconnect(id_);
+      internal::await_ack_cm_event(evt_ch_, RDMA_CM_EVENT_DISCONNECTED);
+    }
+
+    for (const auto& mr_buf : mr_bufs_) rdma_dereg_mr(mr_buf.first);
+
+    rdma_destroy_qp(id_);
+    ibv_destroy_cq(cq_);
+    ibv_dealloc_pd(pd_);
+
+    rdma_destroy_id(id_);
+
+    if (addr_info_) rdma_freeaddrinfo(addr_info_);
+  }
 
   connection& operator=(const connection&) = delete;
-  connection& operator=(connection&& other) noexcept;
 
+  connection& operator=(connection&& other) noexcept {
+    std::swap(evt_ch_, other.evt_ch_);
+    std::swap(id_, other.id_);
+
+    std::swap(pd_, other.pd_);
+    std::swap(cq_, other.cq_);
+
+    std::swap(mr_bufs_, other.mr_bufs_);
+
+    std::swap(addr_info_, other.addr_info_);
+
+    return *this;
+  }
+
+  rdma_event_channel* event_channel() { return evt_ch_; }
   rdma_cm_id* id() { return id_; }
 
-  std::pair<ibv_mr*, void*> make_mr(std::size_t size);
+  ibv_pd* pd() { return pd_; }
+  ibv_cq* cq() { return cq_; }
+
+  std::pair<ibv_mr*, void*> make_mr(std::size_t size) {
+    std::vector<std::uint8_t> buf;
+    buf.reserve(size);
+
+    void* ptr = buf.data();
+
+    ibv_mr* mr = rdma_reg_msgs(id_, ptr, size);
+    if (!mr) return {nullptr, nullptr};
+
+    mr_bufs_[mr] = std::move(buf);
+
+    return {mr, ptr};
+  }
 
   // Deregister a memory region and delete the associated memory buffer.
   //
   // Memory regions are destroyed automatically when `connection` deconstructs.
   // It is not necessary to destroy them manually.
-  void destroy_mr(ibv_mr* mr);
+  void destroy_mr(ibv_mr* mr) {
+    if (!mr_bufs_.contains(mr)) return;
+    rdma_dereg_mr(mr);
+    mr_bufs_.erase(mr);
+  }
 
  protected:
-  explicit connection(rdma_cm_id* id) : id_(id) {}
-
-  // For client-side connection ONLY. DO NOT use for server-side binding
-  // address, otherwise double free will occur.
-  connection(rdma_cm_id* id, rdma_addrinfo* addr_info)
-      : id_(id), addr_info_(addr_info) {}
+  connection(rdma_event_channel* evt_ch, rdma_cm_id* id, ibv_pd* pd, ibv_cq* cq,
+             rdma_addrinfo* addr_info = nullptr)
+      : evt_ch_(evt_ch), id_(id), pd_(pd), cq_(cq), addr_info_(addr_info) {}
 
  private:
-  rdma_cm_id* id_ = nullptr;
+  // DO NOT destroy the event channel in `connection`, its lifetime is bonded
+  // with the server/client.
+  rdma_event_channel* evt_ch_;
+
+  rdma_cm_id* id_;
+
+  ibv_pd* pd_;
+  ibv_cq* cq_;
+
   std::unordered_map<ibv_mr*, std::vector<std::uint8_t>> mr_bufs_;
 
-  // For client-side connection ONLY. DO NOT use for server-side binding
-  // address, otherwise double free will occur.
-  rdma_addrinfo* addr_info_ = nullptr;
+  rdma_addrinfo* addr_info_;
 };
 
 class server {
  public:
-  server(const char* addr, const char* port);
+  server(const char* addr, std::uint16_t port);
   server(const server&) = delete;
   server(server&& other) noexcept;
 
@@ -62,17 +142,17 @@ class server {
   server& operator=(const server&) = delete;
   server& operator=(server&& other) noexcept;
 
-  void listen();
-  connection accept();
+  void listen(int backlog = 0);
+  server_connection accept();
 
  private:
   bool is_inited_ = false;
 
-  rdma_addrinfo* addr_info_;
+  rdma_event_channel* evt_ch_;
   rdma_cm_id* listen_id_;
 };
 
-connection connect(const char* addr, const char* port);
+client_connection connect(const char* addr, const char* port_cstr);
 
 }  // namespace manualmira::rdma
 
--- manualmira/src/rdma.cpp
+++ manualmira/src/rdma.cpp
@@ -1,77 +1,52 @@
 #include "manualmira/rdma.h"
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <cstdlib>
 #include <cstring>
 #include <stdexcept>
 
-#include "rdma/rdma_verbs.h"
-
 namespace manualmira::rdma {
 
-connection::connection(connection&& other) noexcept {
-  std::swap(id_, other.id_);
-  std::swap(mr_bufs_, other.mr_bufs_);
+namespace internal {
 
-  std::swap(addr_info_, other.addr_info_);
+rdma_cm_event* await_cm_event(rdma_event_channel* ch,
+                              rdma_cm_event_type event) {
+  rdma_cm_event* evt;
+  // TODO: handle error events
+  do {
+    if (rdma_get_cm_event(ch, &evt))
+      throw std::runtime_error("Failed to get RDMA CM event");
+  } while (evt->status != 0 || evt->event != event);
+  return evt;
 }
 
-connection::~connection() {
-  if (id_) {
-    rdma_disconnect(id_);
-    for (const auto& mr_buf : mr_bufs_) rdma_dereg_mr(mr_buf.first);
-    rdma_destroy_ep(id_);
+void await_ack_cm_event(rdma_event_channel* ch, rdma_cm_event_type event) {
+  if (rdma_ack_cm_event(await_cm_event(ch, event)))
+    throw std::runtime_error("Failed to acknowledge RDMA CM event");
+}
+
+}  // namespace internal
+
+server::server(const char* addr, std::uint16_t port) {
+  evt_ch_ = rdma_create_event_channel();
+  if (!evt_ch_) throw std::runtime_error("Failed to create RDMA event channel");
+
+  if (rdma_create_id(evt_ch_, &listen_id_, nullptr, RDMA_PS_TCP)) {
+    rdma_destroy_event_channel(evt_ch_);
+    throw std::runtime_error("Failed to create RDMA server listening ID");
   }
 
-  if (addr_info_) rdma_freeaddrinfo(addr_info_);
-}
-
-connection& connection::operator=(connection&& other) noexcept {
-  std::swap(id_, other.id_);
-  std::swap(mr_bufs_, other.mr_bufs_);
-
-  std::swap(addr_info_, other.addr_info_);
-
-  return *this;
-}
-
-std::pair<ibv_mr*, void*> connection::make_mr(std::size_t size) {
-  std::vector<std::uint8_t> buf;
-  buf.reserve(size);
-
-  void* ptr = buf.data();
-
-  ibv_mr* mr = rdma_reg_msgs(id_, ptr, size);
-  if (!mr) return {nullptr, nullptr};
-
-  mr_bufs_[mr] = std::move(buf);
-
-  return {mr, ptr};
-}
-
-void connection::destroy_mr(ibv_mr* mr) {
-  if (!mr_bufs_.contains(mr)) return;
-  rdma_dereg_mr(mr);
-  mr_bufs_.erase(mr);
-}
-
-server::server(const char* addr, const char* port) {
-  rdma_addrinfo hints;
-  std::memset(&hints, 0, sizeof(hints));
-  hints.ai_flags = RAI_PASSIVE;
-  hints.ai_port_space = RDMA_PS_TCP;
-
-  if (rdma_getaddrinfo(addr, port, &hints, &addr_info_))
-    throw std::runtime_error("Failed to get bind address information");
-
-  ibv_qp_init_attr attr;
-  std::memset(&attr, 0, sizeof(attr));
-  attr.cap.max_send_wr = attr.cap.max_recv_wr = 1;
-  attr.cap.max_send_sge = attr.cap.max_recv_sge = 1;
-  attr.cap.max_inline_data = 0;
-  attr.sq_sig_all = true;
-
-  if (rdma_create_ep(&listen_id_, addr_info_, nullptr, &attr)) {
-    rdma_freeaddrinfo(addr_info_);
-    throw std::runtime_error("Failed to create RDMA endpoint");
+  sockaddr_in bind_addr;
+  bind_addr.sin_family = AF_INET;
+  bind_addr.sin_port = htons(port);
+  bind_addr.sin_addr.s_addr = inet_addr(addr);
+  if (rdma_bind_addr(listen_id_, reinterpret_cast<sockaddr*>(&bind_addr))) {
+    rdma_destroy_id(listen_id_);
+    rdma_destroy_event_channel(evt_ch_);
+    throw std::runtime_error("Failed to bind RDMA server to address");
   }
 
   is_inited_ = true;
@@ -80,15 +55,15 @@ server::server(const char* addr, const char* port) {
 server::server(server&& other) noexcept {
   std::swap(is_inited_, other.is_inited_);
 
-  std::swap(addr_info_, other.addr_info_);
+  std::swap(evt_ch_, other.evt_ch_);
   std::swap(listen_id_, other.listen_id_);
 }
 
 server::~server() {
   if (!is_inited_) return;
 
-  rdma_destroy_ep(listen_id_);
-  rdma_freeaddrinfo(addr_info_);
+  rdma_destroy_id(listen_id_);
+  rdma_destroy_event_channel(evt_ch_);
 
   is_inited_ = false;
 }
@@ -96,57 +71,153 @@ server::~server() {
 server& server::operator=(server&& other) noexcept {
   std::swap(is_inited_, other.is_inited_);
 
-  std::swap(addr_info_, other.addr_info_);
+  std::swap(evt_ch_, other.evt_ch_);
   std::swap(listen_id_, other.listen_id_);
 
   return *this;
 }
 
-void server::listen() {
-  if (rdma_listen(listen_id_, 0))
+void server::listen(int backlog) {
+  if (rdma_listen(listen_id_, backlog))
     throw std::runtime_error("Failed to start RDMA listening");
 }
 
-connection server::accept() {
-  rdma_cm_id* conn_id;
-  if (rdma_get_request(listen_id_, &conn_id))
-    throw std::runtime_error("Failed to get RDMA request");
+server_connection server::accept() {
+  rdma_cm_event* conn_req_evt =
+      internal::await_cm_event(evt_ch_, RDMA_CM_EVENT_CONNECT_REQUEST);
+  rdma_cm_id* conn_id = conn_req_evt->id;
 
-  if (rdma_accept(conn_id, nullptr))
+  ibv_pd* pd = ibv_alloc_pd(conn_id->verbs);
+  if (!pd) {
+    rdma_ack_cm_event(conn_req_evt);
+    throw std::runtime_error("Failed to allocate IBV PD");
+  }
+
+  ibv_cq* cq = ibv_create_cq(conn_id->verbs, 1, nullptr, nullptr, 0);
+  if (!cq) {
+    ibv_dealloc_pd(pd);
+    rdma_ack_cm_event(conn_req_evt);
+    throw std::runtime_error("Failed to create IBV CQ");
+  }
+
+  ibv_qp_init_attr attr;
+  std::memset(&attr, 0, sizeof(attr));
+  attr.recv_cq = attr.send_cq = cq;
+  attr.cap.max_send_wr = attr.cap.max_recv_wr = 1;
+  attr.cap.max_send_sge = attr.cap.max_recv_sge = 1;
+  attr.cap.max_inline_data = 0;
+  attr.qp_type = IBV_QPT_RC;
+  attr.sq_sig_all = true;
+  if (rdma_create_qp(conn_id, pd, &attr)) {
+    ibv_destroy_cq(cq);
+    ibv_dealloc_pd(pd);
+    rdma_ack_cm_event(conn_req_evt);
+    throw std::runtime_error("Failed to create RDMA QP");
+  }
+
+  if (rdma_accept(conn_id, nullptr)) {
+    rdma_destroy_qp(conn_id);
+    ibv_destroy_cq(cq);
+    ibv_dealloc_pd(pd);
+    rdma_ack_cm_event(conn_req_evt);
     throw std::runtime_error("Failed to accept RDMA connection");
+  }
 
-  return connection(conn_id);
+  rdma_ack_cm_event(conn_req_evt);
+
+  internal::await_ack_cm_event(evt_ch_, RDMA_CM_EVENT_ESTABLISHED);
+
+  return {evt_ch_, conn_id, pd, cq};
 }
 
-connection connect(const char* addr, const char* port) {
+client_connection connect(const char* addr, const char* port_cstr) {
   rdma_addrinfo hints;
   std::memset(&hints, 0, sizeof(hints));
   hints.ai_port_space = RDMA_PS_TCP;
 
   rdma_addrinfo* addr_info;
-  if (rdma_getaddrinfo(addr, port, &hints, &addr_info))
+  if (rdma_getaddrinfo(addr, port_cstr, &hints, &addr_info))
     throw std::runtime_error("Failed to get server address information");
+
+  rdma_event_channel* evt_ch = rdma_create_event_channel();
+  if (!evt_ch) {
+    rdma_freeaddrinfo(addr_info);
+    throw std::runtime_error("Failed to create RDMA event channel");
+  }
+
+  rdma_cm_id* id;
+  if (rdma_create_id(evt_ch, &id, nullptr, RDMA_PS_TCP)) {
+    rdma_destroy_event_channel(evt_ch);
+    rdma_freeaddrinfo(addr_info);
+    throw std::runtime_error("Failed to create RDMA server listening ID");
+  }
+
+  if (rdma_resolve_addr(id, nullptr, addr_info->ai_dst_addr, 2000)) {
+    rdma_destroy_id(id);
+    rdma_destroy_event_channel(evt_ch);
+    rdma_freeaddrinfo(addr_info);
+    throw std::runtime_error("Failed to resolve remote address");
+  }
+
+  internal::await_ack_cm_event(evt_ch, RDMA_CM_EVENT_ADDR_RESOLVED);
+
+  ibv_pd* pd = ibv_alloc_pd(id->verbs);
+  if (!pd) {
+    rdma_destroy_id(id);
+    rdma_destroy_event_channel(evt_ch);
+    rdma_freeaddrinfo(addr_info);
+    throw std::runtime_error("Failed to allocate IBV PD");
+  }
+
+  ibv_cq* cq = ibv_create_cq(id->verbs, 1, nullptr, nullptr, 0);
+  if (!cq) {
+    ibv_dealloc_pd(pd);
+    rdma_destroy_id(id);
+    rdma_destroy_event_channel(evt_ch);
+    rdma_freeaddrinfo(addr_info);
+    throw std::runtime_error("Failed to create IBV CQ");
+  }
 
   ibv_qp_init_attr attr;
   std::memset(&attr, 0, sizeof(attr));
+  attr.recv_cq = attr.send_cq = cq;
   attr.cap.max_send_wr = attr.cap.max_recv_wr = 1;
   attr.cap.max_send_sge = attr.cap.max_recv_sge = 1;
   attr.cap.max_inline_data = 0;
+  attr.qp_type = IBV_QPT_RC;
   attr.sq_sig_all = true;
-
-  rdma_cm_id* id;
-  if (rdma_create_ep(&id, addr_info, nullptr, &attr)) {
+  if (rdma_create_qp(id, pd, &attr)) {
+    ibv_destroy_cq(cq);
+    ibv_dealloc_pd(pd);
+    rdma_destroy_id(id);
+    rdma_destroy_event_channel(evt_ch);
     rdma_freeaddrinfo(addr_info);
-    throw std::runtime_error("Failed to create RDMA endpoint");
+    throw std::runtime_error("Failed to create RDMA QP");
   }
 
+  if (rdma_resolve_route(id, 2000)) {
+    ibv_destroy_cq(cq);
+    ibv_dealloc_pd(pd);
+    rdma_destroy_id(id);
+    rdma_destroy_event_channel(evt_ch);
+    rdma_freeaddrinfo(addr_info);
+    throw std::runtime_error("Failed to resolve route to remote");
+  }
+
+  internal::await_ack_cm_event(evt_ch, RDMA_CM_EVENT_ROUTE_RESOLVED);
+
   if (rdma_connect(id, nullptr)) {
-    rdma_destroy_ep(id);
+    ibv_destroy_cq(cq);
+    ibv_dealloc_pd(pd);
+    rdma_destroy_id(id);
+    rdma_destroy_event_channel(evt_ch);
     rdma_freeaddrinfo(addr_info);
     throw std::runtime_error("Failed to connect to RDMA remote");
   }
 
-  return {id, addr_info};
+  internal::await_ack_cm_event(evt_ch, RDMA_CM_EVENT_ESTABLISHED);
+
+  return {evt_ch, id, pd, cq, addr_info};
 }
 
 }  // namespace manualmira::rdma
